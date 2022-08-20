5715926196892e4069a8b7afd2669731a47d5910
vovakumov1971 <109413527+vovakumov1971@users.noreply.github.com>
2022-08-20 10:17:24 +0300

    2 commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int value1 = 456;
+int value2 = 782;
+int value3 = 918;
+int result1 = value1 % 10;
+int result2= value2 % 10;
+int result3 = value3 % 10;
+Console.WriteLine($"{value1} -> {result1}");
+Console.WriteLine($"{value2} -> {result2}");
+Console.WriteLine($"{value3} -> {result3}");
+
